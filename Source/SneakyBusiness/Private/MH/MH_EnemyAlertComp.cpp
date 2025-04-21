1610cfb5689d2e54152aebdf29558c35fabd5848
may-04 <146904012+may-04@users.noreply.github.com>
2025-04-21 11:10:41 +0900

    [feat] MH : EnemyAletComp 작업중

--- Source/SneakyBusiness/Private/MH/MH_EnemyAlertComp.cpp
+++ Source/SneakyBusiness/Private/MH/MH_EnemyAlertComp.cpp
@@ -3,6 +3,9 @@
 
 #include "MH/MH_EnemyAlertComp.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "LHM/Enemy.h"
+
 // Sets default values for this component's properties
 UMH_EnemyAlertComp::UMH_EnemyAlertComp()
 {
@@ -35,6 +38,18 @@ void UMH_EnemyAlertComp::TickComponent(float DeltaTime, ELevelTick TickType, FAc
 void UMH_EnemyAlertComp::AlertNearbyEnemies(FVector SourceLocation, float Radius)
 {
 	TArray<AActor*> FoundEnemies;
-	
-}
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AEnemy::StaticClass(), FoundEnemies);
 
+	for (AActor* EnemyActor : FoundEnemies)
+	{
+		float Dist = FVector::Dist(EnemyActor->GetActorLocation(), SourceLocation);
+		if (Dist <= Radius)
+		{
+			AEnemy* Enemy = Cast<AEnemy>(EnemyActor);
+			if (Enemy)
+			{
+				//Enemy->OnAlerted(SourceLocation); // 이 함수는 에너미에 구현돼 있어야겠지
+			}
+		}
+	}
+}
